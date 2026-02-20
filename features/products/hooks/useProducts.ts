5b13a28d650aca059e424080a36b7cba17037996
vader4k <odukwedaniel1@gmail.com>
2026-02-20 19:57:15 +0100

    feat: Introduce product category display, integrate React Query for data management, and update the root layout.

---
+++ features/products/hooks/useProducts.ts
@@ -0,0 +1,26 @@
+import { useQuery } from '@tanstack/react-query'
+import { productServices } from '../services/product.service'
+
+export const useGetProductsByCategory = (category: string) => {
+    return useQuery({
+        queryKey: ['products', category],
+        queryFn: () => productServices.getByCategory(category),
+        enabled: !!category,
+    })
+}
+
+export const useGetProductsBySearch = (search: string) => {
+    return useQuery({
+        queryKey: ['products', search],
+        queryFn: () => productServices.getBySearch(search),
+        enabled: !!search,
+    })
+}
+
+export const useGetProductsByPrice = (price: number) => {
+    return useQuery({
+        queryKey: ['products', price],
+        queryFn: () => productServices.getByPrice(price),
+        enabled: !!price,
+    })
+}
--- sections/home/Categories.tsx
+++ sections/home/Categories.tsx
@@ -1,8 +1,24 @@
-import React from 'react'
+"use client"
+
+import CategoriesTab from "@/features/categories/components/CategoriesTab";
+import { useState } from "react";
+
+export const PRODUCT_CATEGORIES = [
+  "men",
+  "women",
+  "outerwear",
+]
 
 const Categories = () => {
+
+  const [selected, setSelected] = useState(PRODUCT_CATEGORIES[0])
+
   return (
-    <div>Categories</div>
+    <section className="w-full max-w-7xl mx-auto my-14">
+        <div className="w-full">
+            <CategoriesTab selected={selected} setSelected={setSelected} categories={PRODUCT_CATEGORIES} />
+        </div>
+    </section>
   )
 }
 
