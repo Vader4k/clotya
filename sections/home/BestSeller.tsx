15b201863cd90d74926723bd6f9974d018560e88
vader4k <odukwedaniel1@gmail.com>
2026-02-20 12:47:33 +0100

    @add/ discount setup

--- sections/home/BestSeller.tsx
+++ sections/home/BestSeller.tsx
@@ -3,6 +3,7 @@ import ProductCard from "@/features/products/components/ProductCard";
 import { BestSellerProps } from "./types/home.types";
 import Link from "next/link";
 import { MoveRight } from "lucide-react";
+import Discount from "./Discount";
 
 const BestSeller = async ({ variant }: BestSellerProps) => {
 
@@ -17,22 +18,26 @@ const BestSeller = async ({ variant }: BestSellerProps) => {
       </div>
 
       {variant === 'wide' && (
-        <div className="mt-10 flex items-start justify-between gap-6 w-full">
-          <div className="w-[45%]">
-            <ProductCard {...bestSellerProducts[0]} />
+        <>
+          <div className="mt-10 flex items-start justify-between gap-6 w-full">
+            <div className="w-[45%]">
+              <ProductCard {...bestSellerProducts[0]} />
+            </div>
+            <div className="w-[25%] min-h-156 banner3 font-jost p-14 text-white flex flex-col gap-4 items-start justify-center">
+              <span className="font-semibold">Winter 2022 collection</span>
+              <h3 className="text-4xl font-medium">Valentin Paul <br />Essential Collection</h3>
+              <p className="text-sm text-balance font-inter font-light">Discover the latest trends in fashion with our new collection. Handpicked pieces that combine style, comfort, and quality...</p>
+              <Link href={'/shop'} className="mt-4 font-medium flex items-center gap-3">
+                Shop Collection <MoveRight />
+              </Link>
+            </div>
+            <div className="w-[45%]">
+              <ProductCard {...bestSellerProducts[1]} />
+            </div>
           </div>
-          <div className="w-[25%] min-h-156 banner3 font-jost p-14 text-white flex flex-col gap-4 items-start justify-center">
-            <span className="font-semibold">Winter 2022 collection</span>
-            <h3 className="text-4xl font-medium">Valentin Paul <br />Essential Collection</h3>
-            <p className="text-sm text-balance font-inter font-light">Discover the latest trends in fashion with our new collection. Handpicked pieces that combine style, comfort, and quality...</p>
-            <Link href={'/shop'} className="mt-4 font-medium flex items-center gap-3">
-              Shop Collection <MoveRight />
-            </Link>
-          </div>
-          <div className="w-[45%]">
-            <ProductCard {...bestSellerProducts[1]} />
-          </div>
-        </div>
+
+          <Discount />
+        </>
       )}
     </section>
   )
---
+++ sections/home/Discount.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Discount = () => {
+  return (
+    <div className='w-full flex items-center justify-center gap-3 py-5  bg-[#faf0eb]'>
+        <p>Super discount for your <em>first purchase</em>.</p>
+        <p>FREE15FIRST</p>
+        <p>Use discount code in checkout!</p>
+    </div>
+  )
+}
+
+export default Discount
