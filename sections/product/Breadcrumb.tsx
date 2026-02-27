8b0abe9c9dceb54b2cd24991150c54cdfb343420
vader4k <odukwedaniel1@gmail.com>
2026-02-27 19:05:21 +0100

    @add/ font adjustment

--- sections/product/Breadcrumb.tsx
+++ sections/product/Breadcrumb.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 const Breadcrumb = ({category, tags, name}: {category: string[], tags: string[], name: string}) => {
   return (
-    <div className="flex items-center gap-1 text-xs">
+    <div className="flex items-center gap-1 text-xs font-jost">
       <Link href="/" className="text-gray-400 hover:underline">Home</Link>
       <span className="text-gray-400">/</span>
       <Link href={`/shop?category=${category[0]}`} className="text-gray-400 hover:underline capitalize">
